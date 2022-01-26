f6496cd1acbc8011a2d4c553a25be8ff34df9435
babashankar <babashankarsn@gmail.com>
2022-01-26 21:05:49 +0530

    Success Connect to Databse

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-const posts = [];
+const mongoose = require("mongoose");
+
+
+
+// connecting to mongodb
+mongoose.connect("mongodb://127.0.0.1:27017/blogdb");
+const blogSchema = mongoose.Schema({
+    title: String,
+    body: String
+
+});
+const blogpost = mongoose.model("blogpost", blogSchema);
+
+
+
+
+
+
 
 const homecontent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Senectus et netus et malesuada fames. Dictum at tempor commodo ullamcorper a lacus vestibulum. Sit amet nisl suscipit adipiscing bibendum est. Convallis aenean et tortor at risus viverra adipiscing at in. Dolor sit amet consectetur adipiscing elit duis tristique. Enim tortor at auctor urna nunc id cursus metus. Eget dolor morbi non arcu risus quis varius quam quisque. Integer vitae justo eget magna fermentum. Massa sapien faucibus et molestie. Tristique senectus et netus et malesuada fames ac. Aliquet porttitor lacus luctus accumsan tortor posuere ac. Aliquam id diam maecenas ultricies mi. Nisl condimentum id venenatis a condimentum. Consequat ac felis donec et odio pellentesque diam. At quis risus sed vulputate odio ut enim. Ut pharetra sit amet aliquam id diam maecenas ultricies. Malesuada proin libero nunc consequat interdum varius sit. Eget aliquet nibh praesent tristique.";
 const contactcontent = "Varius duis at consectetur lorem donec massa sapien faucibus. Imperdiet proin fermentum leo vel orci. Sollicitudin aliquam ultrices sagittis orci a scelerisque. Sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Enim diam vulputate ut pharetra sit amet aliquam id. Adipiscing bibendum est ultricies integer quis auctor. Porta lorem mollis aliquam ut porttitor leo a. Pretium nibh ipsum consequat nisl vel pretium lectus quam id. Est placerat in egestas erat. Luctus venenatis lectus magna fringilla urna porttitor. Rhoncus aenean vel elit scelerisque mauris pellentesque. Diam phasellus vestibulum lorem sed risus ultricies. Nunc pulvinar sapien et ligula ullamcorper. Erat pellentesque adipiscing commodo elit at imperdiet. Quis vel eros donec ac odio tempor orci dapibus ultrices. Enim nunc faucibus a pellentesque sit. Vitae auctor eu augue ut lectus arcu.";
@@ -16,7 +33,17 @@ app.get("/compose", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    res.render("home", { homecontent: homecontent, posts: posts });
+    blogpost.find({}, (err, posts) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("home", { homecontent: homecontent, posts: posts });
+
+        }
+
+
+    });
+
 });
 app.get("/contact", (req, res) => {
     res.render("contact", { contactcontent: contactcontent });
@@ -28,13 +55,15 @@ app.get("/about", (req, res) => {
 
 
 app.post("/compose", (req, res) => {
-    const title = req.body.post_title;
-    const body = req.body.post_body;
-    const newPost = {
-        post_title: title,
-        post_body: body
-    }
-    posts.push(newPost);
+    const p_title = req.body.post_title;
+    const p_body = req.body.post_body;
+    const new_post = new blogpost({
+        title: p_title,
+        body: p_body
+    });
+    new_post.save();
+
+
     res.redirect("/");
 
 
@@ -43,18 +72,31 @@ app.post("/compose", (req, res) => {
 
 app.get("/posts/:postName", (req, res) => {
     var orgpost = req.params.postName;
-    posts.forEach(post => {
-        if (orgpost === post.post_title) {
-            res.render("post", {
-                req_title: post.post_title,
-                req_body: post.post_body
+    blogpost.findOne({ title: orgpost }, (err, reqpost) => {
+        if (err) {
+            console.log(err);
+        } else {
 
-            });
+            res.render("post", { req_title: reqpost.title, req_body: reqpost.body, reqid: reqpost._id });
         }
+
     });
 });
 
 app.listen(3000, function() {
     console.log("yay working");
+
+});
+app.post("/delete", (req, res) => {
+    let id = req.body.deleteid;
+    blogpost.findByIdAndDelete(id, (err, docs) => {
+        if (err) {
+            console.log(err);
+        } else {
+
+            res.redirect("/");
+        }
+
+    });
 
 });
